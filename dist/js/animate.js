937602729072ee41e1c9eb74b440f38b1c099014
Rene Mumba Kibutika <johndoe71@outlook.com>
2018-10-16 11:24:20 +0200

    Rajout bouton mode

--- dist/js/animate.js
+++ dist/js/animate.js
@@ -34,7 +34,7 @@ anime.timeline({loop: true})
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
-    delay: 1400
+    delay: 7200000
   });
 
 anime({
@@ -44,3 +44,4 @@ anime({
   easing: "linear",
   loop: true
 });
+
